f14fe0ae945ded2f06a49a926690d95947de48a0
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-03-11 17:28:05 +0900

    show/update/destroy機能の大枠まで作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,15 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    if params[:tag].present?
+      @events = Event.joins(:tags).where(tags: { name: params[:tag] })
+    else
+      @events = Event.all
+    end
+      @tags = Tag.all
   end
 
-  # GET /events/1 or /events/1.json
-  def show
-  end
+  def show; end
 
   # GET /events/new
   def new
@@ -16,21 +19,17 @@ class EventsController < ApplicationController
   end
 
   # GET /events/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /events or /events.json
   def create
     @event = Event.new(event_params)
+    @event.host_user_id = 1  # 仮にホストユーザーIDとして1を設定
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: "Event was successfully created." }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to @event, notice: "Event was successfully created."
+    else
+      render :new
     end
   end
 
@@ -65,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :start_time, :end_time, :deadline, :location, :description, :capacity, :host_user_id)
+      params.require(:event).permit(:title, :start_time, :end_time, :deadline, :location, :description, :capacity)
     end
-end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :users
   root "static_pages#top"
   resources :events
 end

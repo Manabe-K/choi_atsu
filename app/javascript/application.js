f347e4111c3925a21b8bdabd60fc144c3e832fbf
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-09 10:06:42 +0900

    デプロイエラー修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
-// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
 import './menu_toggle'
@@ -7,12 +6,10 @@ document.addEventListener('turbo:load', function() {
   const profileButton = document.getElementById('profile-button');
   const dropdownMenu = document.getElementById('dropdown-menu');
 
-  // プロフィールボタンがクリックされたとき
   profileButton.addEventListener('click', function() {
     dropdownMenu.classList.toggle('hidden');
   });
 
-  // メニュー外をクリックしたときにメニューを閉じる
   document.addEventListener('click', function(event) {
     if (!profileButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
       dropdownMenu.classList.add('hidden');

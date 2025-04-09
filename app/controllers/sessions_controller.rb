8b1677fcc2d828f534566d70a28f92679c5220ee
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-09 13:49:19 +0900

    デプロイエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,6 @@ class SessionsController < ApplicationController
       if @user.persisted?
         # 既に登録済み → ログイン状態にしてリダイレクト
         session[:user_id] = @user.id
-        Rails.logger.info "✅ session[:user_id] set to: #{session[:user_id]}"
         redirect_to events_path, notice: "ログインしました。"
       else
         # 新規ユーザー → セッションに一時保存して登録画面へ
@@ -28,7 +27,6 @@ class SessionsController < ApplicationController
           github_token: @user.github_token,
           profile_picture: @user.profile_picture
         }
-        Rails.logger.info "📝 session[:user_registration] set: #{session[:user_registration].inspect}"
         redirect_to new_user_path
       end
     else

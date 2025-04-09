0d614a0beae9d901a02bd1a4692b2725061452c1
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-09 15:13:26 +0900

    デプロイエラー修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user"
 end
 
-OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.allowed_request_methods = %i[post]

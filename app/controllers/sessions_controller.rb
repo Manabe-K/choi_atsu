81c57af56264b1be922070e66b9c9ee78e96281b
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-10 23:16:58 +0900

    デプロイエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
       end
     else
       flash[:error] = "You must be a member of RunTeq to register."
-      redirect_to root_path
+      redirect_to events_path
     end
   end
 

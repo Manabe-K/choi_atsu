ac30a27cb0a3e6c4db1a09086275e7940668f959
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-11 00:53:59 +0900

    デプロイエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def omniauth_callback
     auth_info = request.env["omniauth.auth"]
     @user = User.find_or_initialize_by(github_uid: auth_info["uid"])
+    
     @user.assign_attributes(
       name: auth_info["info"]["name"],
       github_token: auth_info["credentials"]["token"],
@@ -9,20 +10,20 @@ class SessionsController < ApplicationController
     )
 
     if user_is_member_of_runteq?(@user.github_token)
-      if @user.persisted?
-        # ログイン処理
-        session[:user_id] = @user.id
-        redirect_to events_path, notice: "ログインしました。"
-      else
-        # 新規ユーザー登録処理
-        if @user.save
-          # 登録完了後にuser#editにリダイレクトして詳細情報の編集を受け付ける
+      begin
+        if @user.persisted?
+          # ログイン処理
+          session[:user_id] = @user.id
+          redirect_to events_path, notice: "ログインしました。"
+        else
+          # 新規ユーザー登録処理
+          @user.save!
           session[:user_id] = @user.id
           redirect_to edit_user_path(@user), notice: "ユーザー登録が完了しました。詳細情報を入力してください。"
-        else
-          flash[:error] = "ユーザー登録に失敗しました。"
-          redirect_to root_path
         end
+      rescue ActiveRecord::RecordInvalid
+        flash[:error] = "ユーザー登録に失敗しました。"
+        redirect_to root_path
       end
     else
       flash[:error] = "You must be a member of RunTeq to register."

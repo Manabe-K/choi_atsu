fb1d69bad5cc39bad0f5ab8fcd5b1a3f71cfba03
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-10 16:20:47 +0900

    デプロイエラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,9 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  config.action_dispatch.cookies_same_site_protection = :none
   config.force_ssl = true
+  config.ssl_options = { redirect: false }
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }

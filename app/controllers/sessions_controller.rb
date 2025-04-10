ceab0c8f0608cdb2637ad895627cae9b9ebee595
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-11 02:11:29 +0900

    デプロイエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,31 @@
 class SessionsController < ApplicationController
   def omniauth_callback
     auth = request.env['omniauth.auth']
-
-    # GitHubのUIDを使って、ユーザーが存在するか確認（新規ユーザーなら作成、既存ユーザーなら情報更新）
     @user = User.find_or_initialize_by(github_uid: auth['uid'])
 
-    # ユーザーの情報を更新（新規登録でも既存ユーザーでも）
+    # ユーザー情報を更新（新規登録もしくは既存ユーザー）
     @user.assign_attributes(
       name: auth['info']['name'],
       github_token: auth['credentials']['token'],
       profile_picture: auth['info']['image']
     )
 
+    # 新規ユーザーか既存ユーザーかを判定
+    is_new_user = @user.new_record?
+
+    # RunTeqメンバー確認
     if user_is_member_of_runteq?(@user.github_token)
-      if @user.save
-        session[:user_id] = @user.id  # ユーザーをログイン状態に設定
-        
-        # 新規ユーザーの場合はeditページに、既存ユーザーの場合はevents#indexにリダイレクト
-        if @user.new_record?
-          redirect_to edit_user_path(@user), notice: 'ユーザー登録が完了しました。'
-        else
-          redirect_to events_path, notice: 'ログインしました。'
-        end
+      if is_new_user && @user.save
+        session[:user_id] = @user.id  # ログイン状態に設定
+        redirect_to edit_user_path(@user), notice: 'ユーザー登録が完了しました。'
+      elsif !is_new_user && @user.save
+        session[:user_id] = @user.id  # ログイン状態に設定
+        redirect_to events_path, notice: 'ログインしました。'
       else
         redirect_to root_path, alert: 'ユーザー登録に失敗しました。'
       end
     else
-      flash[:alert] = 'You must be a member of RunTeq to register.'
-      redirect_to root_path
+      redirect_to root_path, alert: 'You must be a member of RunTeq to register.'
     end
   end
 
@@ -38,18 +36,13 @@ class SessionsController < ApplicationController
 
   private
 
+  # RunTeqメンバーかどうかのチェック
   def user_is_member_of_runteq?(github_token)
-    begin
-      response = Faraday.get("https://api.github.com/orgs/runteq/members", headers: { "Authorization" => "token #{github_token}" })
-      
-      if response.status == 200
-        return true
-      else
-        return false
-      end
-    rescue Faraday::ConnectionFailed => e
-      Rails.logger.error("RunTeqメンバー確認APIへの接続に失敗しました: #{e.message}")
-      false
-    end
+    response = Faraday.get("https://api.github.com/orgs/runteq/members", 
+                           headers: { "Authorization" => "token #{github_token}" })
+    response.status == 200
+  rescue Faraday::ConnectionFailed => e
+    Rails.logger.error("RunTeqメンバー確認APIへの接続に失敗しました: #{e.message}")
+    false
   end
 end

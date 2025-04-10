a4f72165b64a0ada4685b7fafa9ce178023b045e
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-11 01:55:21 +0900

    デプロイエラー修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,39 +1,39 @@
 class SessionsController < ApplicationController
   def omniauth_callback
-    auth_info = request.env["omniauth.auth"]
-    @user = User.find_or_initialize_by(github_uid: auth_info["uid"])
-    
+    auth = request.env['omniauth.auth']
+
+    # GitHubのUIDを使って、ユーザーが存在するか確認（新規ユーザーなら作成、既存ユーザーなら情報更新）
+    @user = User.find_or_initialize_by(github_uid: auth['uid'])
+
+    # ユーザーの情報を更新（新規登録でも既存ユーザーでも）
     @user.assign_attributes(
-      name: auth_info["info"]["name"],
-      github_token: auth_info["credentials"]["token"],
-      profile_picture: auth_info["info"]["image"]
+      name: auth['info']['name'],
+      github_token: auth['credentials']['token'],
+      profile_picture: auth['info']['image']
     )
 
     if user_is_member_of_runteq?(@user.github_token)
-      begin
-        if @user.persisted?
-          # ログイン処理
-          session[:user_id] = @user.id
-          redirect_to events_path, notice: "ログインしました。"
+      if @user.save
+        session[:user_id] = @user.id  # ユーザーをログイン状態に設定
+        
+        # 新規ユーザーの場合はeditページに、既存ユーザーの場合はevents#indexにリダイレクト
+        if @user.new_record?
+          redirect_to edit_user_path(@user), notice: 'ユーザー登録が完了しました。'
         else
-          # 新規ユーザー登録処理
-          @user.save!
-          session[:user_id] = @user.id
-          redirect_to edit_user_path(@user), notice: "ユーザー登録が完了しました。詳細情報を入力してください。"
+          redirect_to events_path, notice: 'ログインしました。'
         end
-      rescue ActiveRecord::RecordInvalid
-        flash[:error] = "ユーザー登録に失敗しました。"
-        redirect_to root_path
+      else
+        redirect_to root_path, alert: 'ユーザー登録に失敗しました。'
       end
     else
-      flash[:error] = "You must be a member of RunTeq to register."
+      flash[:alert] = 'You must be a member of RunTeq to register.'
       redirect_to root_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "ログアウトしました"
+    redirect_to root_path, notice: 'ログアウトしました'
   end
 
   private
@@ -41,7 +41,12 @@ class SessionsController < ApplicationController
   def user_is_member_of_runteq?(github_token)
     begin
       response = Faraday.get("https://api.github.com/orgs/runteq/members", headers: { "Authorization" => "token #{github_token}" })
-      response.status == 200
+      
+      if response.status == 200
+        return true
+      else
+        return false
+      end
     rescue Faraday::ConnectionFailed => e
       Rails.logger.error("RunTeqメンバー確認APIへの接続に失敗しました: #{e.message}")
       false
--- db/seeds/guest_user.rb
+++
@@ -1,5 +0,0 @@
-guest_user = User.find_or_create_by!(name: "ゲストユーザー") do |user|
-  user.github_uid = "guest_uid"
-  user.profile_picture = "https://example.com/guest.png"
-  user.github_token = "dummy_token"
-end

186419c942f72f4597f1a49977f9007ba47ae6c9
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-13 16:59:20 +0900

    ログイン機能のリファクタリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,45 +1,38 @@
 class SessionsController < ApplicationController
   def omniauth_callback
-    puts "=== omniauth_callback start ==="
-
     auth = request.env['omniauth.auth']
-    puts "Auth received: UID=#{auth['uid']}, Name=#{auth['info']['name']}"
 
-    @user = User.find_or_initialize_by(github_uid: auth['uid'])
-
-    @user.assign_attributes(
-      name: auth['info']['name'],
-      github_token: auth['credentials']['token'],
-      profile_picture: auth['info']['image']
-    )
-
-    is_new_user = @user.new_record?
-    puts "Is new user: #{is_new_user}"
-
-    # GitHubのusername（nickname）を使ってRunTeqのメンバーか確認
     github_username = auth.info.nickname
     is_member = GithubOrgMemberCheckService.new(github_username: github_username).member?
-    puts "RunTeq member check: #{is_member}"
 
     if is_member
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_path = is_new_user ? edit_user_path(@user) : events_path
-        redirect_to redirect_path, notice: is_new_user ? 'ユーザー登録が完了しました。' : 'ログインしました。'
+      user = User.find_by(github_uid: auth['uid'])
+
+      if user
+        session[:user_id] = user.id
+        redirect_to events_path, notice: 'ログインしました。'
       else
-        puts "User save failed: #{@user.errors.full_messages}"
-        redirect_to root_path, alert: 'ユーザー登録に失敗しました。'
+        # ユーザー登録情報をセッションに保存して、users#newへ
+        session[:user_registration] = {
+          name: auth.info.name,
+          github_uid: auth.uid,
+          github_token: auth.credentials.token,
+          profile_picture: auth.info.image
+        }
+        redirect_to new_user_path, notice: 'ユーザー登録を完了してください。'
       end
     else
-      puts "Not a RunTeq member, redirecting to root"
-      redirect_to root_path, alert: 'You must be a member of RunTeq to register.'
+      redirect_to root_path, alert: 'RunTeqメンバーのみ登録可能です。'
     end
-
-    puts "=== omniauth_callback end ==="
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: 'ログアウトしました'
+  end
+
+  def back_to_users
+    session.delete(:user_registration)
+    redirect_to root_path, notice: "ユーザー登録をキャンセルしました。"
   end
 end

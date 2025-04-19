54c21ee2354f8d3695d2281cadf38b996ae7e463
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-19 19:08:46 +0900

    カレンダーの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -41,6 +41,7 @@ function initializeFlatpickr() {
     flatpickr(startInput, {
       ...baseOptions,
       minDate: oneHourLater,
+      appendTo: startInput.parentElement,
       onChange(selectedDates) {
         if (selectedDates.length > 0) {
           const start = selectedDates[0]

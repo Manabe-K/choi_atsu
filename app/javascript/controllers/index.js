09b108ba31852d9b198a5512260ee5a87f12a828
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-04-20 01:01:46 +0900

    eventsフォームの修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,8 @@
 import { application } from "./application"
 
+import FlatpickrController from "./flatpickr_controller"
+application.register("flatpickr", FlatpickrController)
+
 import FormAnimationController from "./form_animation_controller"
 application.register("form-animation", FormAnimationController)
 

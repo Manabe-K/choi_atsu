a5f34608dfae93f631458fae3e571ba6249c66e0
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-06-05 15:47:04 +0900

    デプロイエラー修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,14 +1,10 @@
-#!/usr/bin/env bash
 set -o errexit
 
-# Install JS dependencies
-yarn install --frozen-lockfile
+# JS & CSS ビルド
+yarn build
+yarn build:css
 
-# Use local bin path for JS tools
-./node_modules/.bin/esbuild app/javascript/*.* --bundle --sourcemap --format=esm --outdir=app/assets/builds --public-path=/assets --loader:.js=jsx
-./node_modules/.bin/tailwindcss -i ./app/assets/stylesheets/application.tailwind.css -o ./app/assets/builds/application.css --minify
-
-# Install Ruby dependencies and setup app
+# Rails のアセットとDB準備
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean

d2f79d0b283530a3c3bacf0c594439c98c91ea6e
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-06-05 15:43:16 +0900

    デプロイエラー修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
 set -o errexit
+
+# Node.js の依存関係を先にインストールしておく（安全策）
+yarn install --frozen-lockfile
 
 # JS & CSS ビルド
 yarn build
 yarn build:css
 
-# Rails のアセットとDB準備
+# Rails の依存関係とアセット & DB
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean

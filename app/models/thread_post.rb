66ed3b70b03318c07c41cc03ff17480e2ebebb62
Manabe-K <kattsu.7ma93nb@gmail.com>
2025-06-06 17:51:13 +0900

    LINE風チャット実装

--- app/models/thread_post.rb
+++ app/models/thread_post.rb
@@ -1,4 +1,8 @@
+# app/models/thread_post.rb
 class ThreadPost < ApplicationRecord
-  belongs_to :user
   belongs_to :event
+  belongs_to :user
+
+  validates :content, presence: true
+  validates :content, format: { without: /\A\s*\z/, message: "を入力してください" }
 end
